a290d6505e33e6592f409e1a45acc387d191fc0b
MostafaAbdElFatah <m.abdelfatah.fci@gmail.com>
2018-12-27 10:15:52 +0200

    first commit

---
+++ ProductProject/Controllers/ProductController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ProductProject.Models;
+using ProductProject.Models.dbEntity;
+
+namespace ProductProject.Controllers
+{
+    public class ProductController : ApiController
+    {
+        DatabaseManager db = new DatabaseManager();
+
+        public List<Product> Get()
+        {
+            return db.getAllProducts();
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            var product = db.getAllProducts().Where(x => x.id == id);
+            if (product == null)
+                return NotFound();
+            else
+                return Ok(product);
+        }
+
+        public IHttpActionResult Post(Product updateProduct)
+        {
+            if (updateProduct != null)
+            {
+                db.updateProduct(updateProduct);
+                return Ok(updateProduct);
+            }
+            else
+            {
+                return NotFound();
+            }
+
+
+        }
+
+        public IHttpActionResult Put(Product newProduct){
+            
+            if (newProduct != null)
+            {
+                db.addProduct(newProduct);
+                return Ok(newProduct);
+            }
+            else {
+                return NotFound();
+            }
+
+        }
+
+        public IHttpActionResult Delete(int id) {
+            var productlist = db.getAllProducts().Where(x => x.id == id);
+            if (productlist != null && productlist.Count() > 0)
+            {
+                Product product = productlist.First();
+
+                db.deleteProduct(product);
+                return Ok(product);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ ProductProject/Models/DatabaseManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ProductProject.Models.dbEntity;
+
+namespace ProductProject.Models
+{
+    class DatabaseManager
+    {
+        ProductContext db = new ProductContext();
+
+        // fetch data
+        public List<Product> getAllProducts()
+        {
+            try
+            {
+                return db.Products.ToList();
+            }
+            catch (Exception ex)
+            {
+                return null;
+                throw;
+            }
+        }
+
+        // create data
+        public bool addProduct(Product product)
+        {
+            try
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                return false;
+                throw;
+            }
+            return true;
+        }
+
+        // update data
+        public bool updateProduct(Product product)
+        {
+            try
+            {
+                db.Products.Add(product);
+                db.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                return false;
+                throw;
+            }
+            return true;
+        }
+
+        // delete data
+        public bool deleteProduct(Product product)
+        {
+            try
+            {
+                db.Products.Remove(product);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                return false;
+                throw;
+            }
+            return true;
+        }
+
+    }
+}
